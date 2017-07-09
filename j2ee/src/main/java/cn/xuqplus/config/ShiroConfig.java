2de84228cb7054b562655c0b71f7ae945ee19ccf
xuqplus <445172495@qq.com>
2017-07-10 01:13:47 +0800

    shiro暂时不拦截

---
+++ j2ee/src/main/java/cn/xuqplus/config/ShiroConfig.java
@@ -0,0 +1,53 @@
+package cn.xuqplus.config;
+
+import cn.xuqplus.shiro.MyAuthorizingRealm;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.realm.Realm;
+import org.apache.shiro.spring.web.ShiroFilterFactoryBean;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by qqx on 2017/7/8.
+ */
+@Configuration
+public class ShiroConfig {
+    @Bean
+    public ShiroFilterFactoryBean shiroFilterFactoryBean() {
+        ShiroFilterFactoryBean factoryBean = new ShiroFilterFactoryBean();
+        /**
+         * 登录页面
+         */
+        factoryBean.setLoginUrl("/login.html");
+
+        /**
+         * 过滤规则
+         */
+        Map definitions = new HashMap();
+        definitions.put("/api/login", "anon");
+        definitions.put("/api/public", "anon");
+        definitions.put("/**", "anon");
+        factoryBean.setFilterChainDefinitionMap(definitions);
+        /**
+         * 安全管理器
+         */
+        factoryBean.setSecurityManager(securityManager());
+        return factoryBean;
+    }
+
+    @Bean//(name = "securityManager")
+    public SecurityManager securityManager() {
+        DefaultWebSecurityManager securityManager = new DefaultWebSecurityManager();
+        securityManager.setRealm(realm());
+        return securityManager;
+    }
+
+    @Bean//(name = "realm")
+    public Realm realm() {
+        return new MyAuthorizingRealm();
+    }
+}
---
+++ j2ee/src/main/java/cn/xuqplus/shiro/MyAuthorizingRealm.java
@@ -0,0 +1,56 @@
+package cn.xuqplus.shiro;
+
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.AuthenticationInfo;
+import org.apache.shiro.authc.AuthenticationToken;
+import org.apache.shiro.authc.SimpleAuthenticationInfo;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.Permission;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.authz.permission.AllPermission;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * Created by Administrator on 2017-04-08.
+ */
+public class MyAuthorizingRealm extends AuthorizingRealm {
+    @Override
+    protected AuthenticationInfo doGetAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        /**
+         * subject.login(token)中的token需要和这里的一致
+         * ,才能登录成功
+         */
+        AuthenticationInfo info = new SimpleAuthenticationInfo("xqq", "123", getName());
+        return info;
+    }
+
+    @Override
+    protected AuthorizationInfo doGetAuthorizationInfo(PrincipalCollection principalCollection) {
+        SimpleAuthorizationInfo authorizationInfo = new SimpleAuthorizationInfo();
+        /**
+         * 角色
+         */
+        Set<String> roles = new HashSet<>();
+        roles.add("admin");
+        //authorizationInfo.setRoles(roles);
+        /**
+         * 对象权限
+         */
+        Set<Permission> permissions = new HashSet<>();
+        permissions.add(new AllPermission());
+        //authorizationInfo.setObjectPermissions(permissions);
+
+        /**
+         * 字符权限
+         */
+        Set<String> strings = new HashSet<>();
+        strings.add("admin:read");
+        strings.add("admin:create");
+        authorizationInfo.setStringPermissions(strings);
+        return authorizationInfo;
+    }
+}
