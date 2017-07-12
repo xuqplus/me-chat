610ef195fa79a8616cbc5a70b106c4cb8b21d8e8
xuqplus <445172495@qq.com>
2017-07-12 12:46:59 +0800

    测试mybatis

--- j2ee/src/main/java/cn/xuqplus/controller/DemoController.java
+++ j2ee/src/main/java/cn/xuqplus/controller/DemoController.java
@@ -1,5 +1,8 @@
 package cn.xuqplus.controller;
 
+import cn.xuqplus.domain.User;
+import cn.xuqplus.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,13 +13,17 @@ import java.util.Map;
  * Created by Administrator on 2017-06-20.
  */
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/demo")
 public class DemoController {
-    @RequestMapping("/demo")
+    @Autowired
+    private UserMapper userMapper;
+
+    @RequestMapping("/findUser")
     public Map demo() {
         Map result = new HashMap();
         result.put("success", true);
-        result.put("msg", "ddddd");
+        User user = userMapper.findOne();
+        result.put("user", user);
         return result;
     }
 }
